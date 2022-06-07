cb9e7eda473b64631c31d66b8d896dd1e187cf2d
Bhagyalakshmi K <bhagyalakshmi.k@nagarro.com>
2022-06-07 13:48:36 +0530

    first commit

---
+++ src/main/java/com/demo/springazuredemo1/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.demo.springazuredemo1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/api")
+    public String getMessage() {
+        return  "First app deployed !!";
+    }
+}
---
+++ src/test/java/com/demo/springazuredemo1/SpringAzureDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springazuredemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
